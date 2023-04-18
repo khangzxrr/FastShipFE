53408f848abb4f1402affa23658379272eb86b05
khangzxrr@gmail.com <khangzxrr@gmail.com>
2023-04-18 23:27:27 +0700

    auto load token in axios

--- src/features/axiosProfile.js
+++ src/features/axiosProfile.js
@@ -1,10 +1,20 @@
 import axios from "axios"
-
-export const API_BASE_URL = "https://fastship.sontran.us"
-//export const API_BASE_URL = "http://localhost:57679"
+import store from '../store';
+//export const API_BASE_URL = "https://fastship.sontran.us"
+export const API_BASE_URL = "http://localhost:57679"
 
 const axiosProfile = axios.create({
     baseURL: API_BASE_URL
 })
+
+axiosProfile.interceptors.request.use((config) => {
+    const { token } = store.getState().login
+    config.headers.Authorization = `Bearer ${token}`
+
+    console.log("set token!!! ", token)
+
+    return config
+})
+
 
 export default axiosProfile
