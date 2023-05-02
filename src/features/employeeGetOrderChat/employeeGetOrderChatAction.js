bdf7bba6a5e58030de32e8672dd4cea8c06491e7
khangzxrr <khangzxrr@gmail.com>
2023-05-02 20:15:38 +0700

    remove authorize header

--- src/features/employeeGetOrderChat/employeeGetOrderChatAction.js
+++ src/features/employeeGetOrderChat/employeeGetOrderChatAction.js
@@ -2,13 +2,8 @@ import axiosProfile from "../axiosProfile"
 import { employeeGetOrderChatSuccessfully } from "./employeeGetOrderChatSlice"
 
 
-export const employeeGetOrderChatAction = (orderId, token) => async dispatch => {
-    const response = await axiosProfile.get(`employee/orders/${orderId}/chats`, {
-        headers: {
-            "Content-type": "application/json",
-            "Authorization": `Bearer ${token}`
-        }
-    })
+export const employeeGetOrderChatAction = (orderId) => async dispatch => {
+    const response = await axiosProfile.get(`employee/orders/${orderId}/chats`)
 
     dispatch(employeeGetOrderChatSuccessfully(response.data))
 
