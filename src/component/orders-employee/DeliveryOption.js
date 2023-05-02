0d524df7627c45141b01a2946ca1ea6933777834
khangzxrr <khangzxrr@gmail.com>
2023-05-02 20:13:52 +0700

    add markFinishBy3rdDeliver and refresh component

--- src/component/orders-employee/DeliveryOption.js
+++ src/component/orders-employee/DeliveryOption.js
@@ -2,10 +2,13 @@ import React from 'react'
 import { Button } from 'antd';
 import { useDispatch } from 'react-redux';
 import { employeeCreateOrderShippingAction } from '../../features/employeeCreateOrderShipping/employeeCreateOrderShippingAction';
+import { employeeGetOrderByIdAction } from '../../features/employeeGetOrderById/employeeGetOrderByIdAction';
+import { employeeMarkFinishDeliveryBy3RdAction } from '../../features/employeeMarkFinishDeliveryBy3rd/employeeMarkFinishDeliveryBy3rdAction';
+import { employeeGetOrderChatAction } from '../../features/employeeGetOrderChat/employeeGetOrderChatAction';
 
 
 export default function DeliveryOption(props) {
-    
+
 
     const dispatch = useDispatch()
 
@@ -14,6 +17,10 @@ export default function DeliveryOption(props) {
             .then((response) => {
 
                 if (response.message === "OK") {
+
+                    dispatch(employeeGetOrderByIdAction(props.order.orderId))
+                    dispatch(employeeGetOrderChatAction(props.order.orderId))
+
                     alert('Đã giao đơn hàng cho shipper thành công');
                 }
             })
@@ -23,34 +30,56 @@ export default function DeliveryOption(props) {
             })
     }
 
+    function markFinishBy3rdDeliver() {
+        dispatch(employeeMarkFinishDeliveryBy3RdAction(props.order.orderId))
+            .then(() => {
+
+                dispatch(employeeGetOrderByIdAction(props.order.orderId))
+                dispatch(employeeGetOrderChatAction(props.order.orderId))
+
+                alert('đã điều chỉnh trạng thái thành giao hàng thành công!');
+            })
+    }
+
 
     return (
         <>
-            <div style={{
-                width: '100%', border: '1px solid grey',
-                borderRadius: '20px', marginBottom: '30px', padding: '20px 20%', border: 'none', boxShadow: ' rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px'
-            }}>
-                {
-                    props.order.localOrderShippingStatus === "notInQueue" && (
-                        <div>
-                            <Button style={{ color: 'black', width:'100%', marginBottom:'10px' }} type="primary" onClick={() => deliveryByShipper(false)}>
-                                Giao hàng bằng nhân viên
-                            </Button>
-                            <Button style={{ color: 'black',width:'100%'}} type="primary" onClick={() => deliveryByShipper(true)}>
-                                Giao hàng bằng bên thứ 3
-                            </Button>
-                        </div>
-                    )
-                }
-                {
-                props.order.localOrderShippingStatus !== "notInQueue"  && (
-                    <span>{props.order.localOrderShippingStatus === "inQueue" ? "Đang trong hàng chờ shipper nhận đơn" : "Shipper đã nhận đơn"} </span>
-                )
-                }
-                    
-                
+            {
+                (props.order.localOrderShippingStatus !== 'delivered' && props.order.status === 'inVNwarehouse') &&
+                (<div style={{
+                    width: '100%', border: '1px solid grey',
+                    borderRadius: '20px', marginBottom: '30px', padding: '20px 20%', border: 'none', boxShadow: ' rgba(0, 0, 0, 0.02) 0px 1px 3px 0px, rgba(27, 31, 35, 0.15) 0px 0px 0px 1px'
+                }}>
+                    {
+                        props.order.localOrderShippingStatus === "notInQueue" && (
+                            <div>
+                                <Button style={{ color: 'black', width: '100%', marginBottom: '10px' }} type="primary" onClick={() => deliveryByShipper(false)}>
+                                    Giao hàng bằng nhân viên
+                                </Button>
+                                <Button style={{ color: 'black', width: '100%' }} type="primary" onClick={() => deliveryByShipper(true)}>
+                                    Giao hàng bằng bên thứ 3
+                                </Button>
+                            </div>
+                        )
+                    }
+                    {
+                        props.order.localOrderShippingStatus !== "notInQueue" && (
+                            <div>
+                                <span>{props.order.localOrderShippingStatus === "inQueue" ? "Đang trong hàng chờ shipper nhận đơn" : "Shipper đã nhận đơn"} </span>
 
-            </div>
+                                {
+                                    props.order.localOrderShippingStatus === "assignedTo3rdShipper" && (
+                                        <Button style={{ color: 'black', width: '100%' }} type="primary" onClick={() => markFinishBy3rdDeliver()}>
+                                            Đánh dấu giao thành công
+                                        </Button>
+                                    )
+                                }
+                            </div>
+                        )
+                    }
+                </div>)
+            }
+
         </>
 
     )
