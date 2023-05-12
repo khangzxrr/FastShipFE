f027dc877dd706931eda2665da4536a09b7334ff
khangzxrr <khangzxrr@gmail.com>
2023-05-13 02:36:57 +0700

    redirect base on role

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,7 +1,23 @@
+import { useNavigate } from "react-router-dom"
 import ContentMiddle from "../component/home/ContentMiddle"
 import { ContentUpper } from "../component/home/ContentUpper"
+import { useSelector } from "react-redux"
+import { useEffect } from "react"
 
 const HomePage = () => {
+
+    const navigate = useNavigate()
+    const { roleName } = useSelector(state => state.login)
+
+    useEffect(() => {
+        if (roleName === "MANAGER"){
+            navigate("/manager-home")
+        } else
+        if (roleName === "EMLPOYEE"){
+            navigate("/employee-order")
+        }
+    }, [roleName]);
+
     return (
         <>
             <div style={{ background: "url(/homebg11.jpg) 0 no-repeat", backgroundSize:'100%'}}>
--- src/pages/manager/manager-home.js
+++ src/pages/manager/manager-home.js
@@ -1,10 +1,30 @@
-import Currency from "../../component/manager/Currency"
+
+import { useDispatch, useSelector } from "react-redux"
 import ManagerMenu from "../../component/manager/ManagerMenu"
 import OrderManager from "../../component/manager/OrderManager"
 import Revenue from "../../component/manager/Revenue"
 import ShipperManager from "../../component/manager/ShipperManager"
 import "../manager/manager.css"
+import { useEffect } from "react"
+import { useNavigate } from "react-router-dom"
+import { logout } from "../../features/login/loginSlice";
 const ManagerHome = () => {
+
+    const { roleName } = useSelector(state => state.login)
+    {}
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        console.log(roleName)
+        if (roleName !== 'MANAGER') {
+            alert('bạn không có quyền xem trang này, vui lòng đăng nhập lại')
+            dispatch(logout())
+            navigate('/login')
+        }
+    }, [roleName])
+
     return (
         <div className="container">
             <ManagerMenu />
