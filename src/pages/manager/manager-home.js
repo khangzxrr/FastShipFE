ce4969808ed1bf99a3953977972d5eb7487cf67f
khangzxrr <khangzxrr@gmail.com>
2023-05-14 00:49:36 +0700

    add total payment cost feature

--- src/pages/manager/manager-home.js
+++ src/pages/manager/manager-home.js
@@ -3,26 +3,38 @@ import { useDispatch, useSelector } from "react-redux"
 import ManagerMenu from "../../component/manager/ManagerMenu"
 import Revenue from "../../component/manager/Revenue"
 import "../manager/manager.css"
-import { useEffect } from "react"
+import { useEffect, useState } from "react"
 import { useNavigate } from "react-router-dom"
 import { logout } from "../../features/login/loginSlice";
 import Order from "../../component/manager/Order"
 import Shipper from "../../component/manager/Shipper"
+import { managerGetTotalPaymentCost } from "../../features/managerGetTotalPaymentCost/managerGetTotalPaymentCost"
 const ManagerHome = () => {
 
     const { roleName } = useSelector(state => state.login)
-    { }
+
+    const [totalPayment, setTotalPayment] = useState(0.0)
+    
 
     const navigate = useNavigate()
     const dispatch = useDispatch()
 
     useEffect(() => {
-        console.log(roleName)
+        
         if (roleName !== 'MANAGER') {
             alert('bạn không có quyền xem trang này, vui lòng đăng nhập lại')
             dispatch(logout())
             navigate('/login')
+            return
         }
+
+        dispatch(managerGetTotalPaymentCost())
+            .then(response => {
+                console.log(response.totalPayment)
+                setTotalPayment(response.totalPayment)
+            })
+
+
     }, [roleName])
 
     return (
@@ -30,7 +42,7 @@ const ManagerHome = () => {
             <ManagerMenu />
             <div style={{ display: 'flex', marginTop: '20px' }}>
                 <div style={{ width: '32%' }}>
-                    <Revenue />
+                    <Revenue totalPayment={totalPayment} />
                 </div>
                 <div style={{ width: '32%', marginLeft: '2%', marginRight: '2%' }}>
                     <Shipper/>
