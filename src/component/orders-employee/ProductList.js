d286e82077838e62d6397a78ed5dd219de607746
khangzxrr <khangzxrr@gmail.com>
2023-05-15 14:49:57 +0700

    only show update weight inVNwarehouse

--- src/component/orders-employee/ProductList.js
+++ src/component/orders-employee/ProductList.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import "../orders-employee/productCard.css"
 import UpdateWeight from './UpdateWeight'
+import { Utils } from '../../features/utils/Utils'
 export default function ProductList(props) {
   return (
     <>
@@ -14,15 +15,23 @@ export default function ProductList(props) {
                   <p ><img src={od.product.imageUrl} alt={od.product.name} style={{ float: 'left' }} />
                     <span style={{ fontSize: 20, fontWeight: 500 }}>{od.product.name}</span><br /><a href={od.product.url}>Link Product</a>
                     <br /><button style={{fontWeight:600, backgroundColor:'yellow', height:'20px', border:'none', borderRadius:'5px'}}>Gía:${od.productCost}</button><br/>Phụ thu: ${od.additionalCost}<br />Phí xử lí: ${od.processCost}<br />Số lượng: {od.quantity}
-                    <br />Phí ship đến kho US: ${od.shipCost}<br />Phí trọng lượng 1kg: ${od.product.costPerWeight}<br />
+                    <br />Phí ship đến kho US: ${od.shipCost}<br />
+                    <span>Khối lượng: {od.product.weight}kg</span><br/>
+                    Phí trọng lượng 1kg: ${od.product.costPerWeight}<br />
+                    Tỉ giá đổi sang VNĐ: {od.product.productCurrencyExchangeRecord.currencyName} | {Utils.formatToVNDCurrency(od.product.productCurrencyExchangeRecord.rate)}<br/>
                     Bảo hành: {od.product.warrantable ? 'có' : 'không'}<br />
                     {od.product.warrantable &&
                       (<span>Mô tả bảo hành: {od.product.warrantableDescription}</span>)}
                     <span>Đổi trả: {od.product.returnable ? 'có' : 'không'}</span><br />
                     {od.product.returnable &&
                       (<span>Mô tả đổi trả: Chấp nhận {od.product.returnDuration} ngày đổi trả</span>)}
-                      <br/><span>Khối lượng:</span>
-                      <UpdateWeight/>
+                      
+
+                      {
+                        props.order.status === 'inVNwarehouse' &&
+                          (<UpdateWeight orderId={props.order.orderId} productId={od.product.id} />)
+                      }
+                      
                   </p>
                 </div>
               </li>
