83ba7690f8f17ce2c9676c36d480498b1cee132e
khangzxrr <khangzxrr@gmail.com>
2023-05-21 01:13:45 +0700

    remove token

--- src/features/getOrderById/getOrderByIdAction.js
+++ src/features/getOrderById/getOrderByIdAction.js
@@ -3,13 +3,10 @@ import { getOrderByIdSuccessfully } from "./getOrderByIdSlice"
 
 
 
-export const getOrderByIdAction = (orderId, token) => async dispatch => {
-    const response = await axiosProfile.get(`/orders/${orderId}`, {
-        headers: {
-            "Content-type": "application/json",
-            "Authorization": `Bearer ${token}`
-        }
-    })
+export const getOrderByIdAction = (orderId) => async dispatch => {
+    const response = await axiosProfile.get(`/orders/${orderId}`)
 
     dispatch(getOrderByIdSuccessfully(response.data))
+
+    return Promise.resolve()
 }
