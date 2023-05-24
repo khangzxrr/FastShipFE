772ab65a58140b17a2ceb7e0ed9d6f808b21cbb4
kduy1412 <kduy1412@gmail.com>
2023-05-24 20:18:45 +0700

    employee request fe

--- src/component/request-employee/EmployeeRequestProducts.js
+++ src/component/request-employee/EmployeeRequestProducts.js
@@ -1,51 +1,71 @@
 import React from 'react'
-import { Input, Button, Select, Descriptions } from 'antd'
+import { Input, Button, Select, Descriptions, Modal } from 'antd'
+import { useState } from 'react';
 export default function EmployeeRequestProducts() {
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const showModal = () => {
+    setIsModalOpen(true);
+  };
+  const handleOk = () => {
+    setIsModalOpen(false);
+  };
+  const handleCancel = () => {
+    setIsModalOpen(false);
+  };
   const handleChange = (value) => {
     console.log(`selected ${value}`);
   };
   return (
-    <div className='requestproducts' style={{ display: 'flex', padding: '10px' }}>
-      <div>
+    <div className='requestproducts' style={{ display: 'flex', padding: '10px', marginRight:'2%' }}>
+      <div style={{width:'28%'}}>
         <img
           src="/service2.jpg"
           style={{
             height: '150px',
             width: '100%',
-            marginTop: '20px'
+            position:'relative',
+            marginTop:'20px'
           }}
         />
-        <p>Giá sản phẩm:</p>
-        <p>Phí xử lý:</p>
-        <p>Phí phụ thu:</p>
-        <p>SL:</p>
-        <p>Note:</p>
+        <Button type='primary' style={{ color: 'black',width:'100%'}} onClick={showModal}>Insert</Button>
+        <Modal title="INSERT" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
+          <Descriptions layout="vertical">
+            <Descriptions.Item label="Giá sản phẩm" ><Input /></Descriptions.Item>
+            <Descriptions.Item label="Phí xử lý" ><Input /></Descriptions.Item>
+            <Descriptions.Item label="Phí phụ thu" ><Input /></Descriptions.Item>
+            <Descriptions.Item label="Ghi chú" span={2}><Input /></Descriptions.Item>
+            <Descriptions.Item label="SL"><Select
+              defaultValue="1"
+              style={{
+                width: '100%',
+              }}
+              onChange={handleChange}
+              options={[
+                {
+                  value: '1',
+                  label: '1',
+                },
+              ]}
+            /></Descriptions.Item>
+          </Descriptions>
+        </Modal>
       </div>
-
-      <div style={{ padding: '0px 10px', width: '100%' }}>
-        <h2>Name:</h2>
-        <Descriptions>
-          <Descriptions.Item label="Type" span={2}></Descriptions.Item>
-          <Descriptions.Item label="SL"><Select
-            defaultValue="1"
-            style={{
-              width: '100%',
-            }}
-            onChange={handleChange}
-            options={[
-              {
-                value: '1',
-                label: '1',
-              },
-            ]}
-          /></Descriptions.Item>
-          <Descriptions.Item label="Giá sản phẩm" span={3}><Input/></Descriptions.Item>
-          <Descriptions.Item label="Phí xử lý" span={3}><Input /></Descriptions.Item>
-          <Descriptions.Item label="Phí phụ thu" span={3}><Input /></Descriptions.Item>
-          <Descriptions.Item label="Ghi chú" span={3}><Input /></Descriptions.Item>
-        </Descriptions>
-        <Button type='primary' style={{ color: 'black', float: 'right' }}>UPDATE</Button>
+      <div style={{ padding: '0px 10px', width: '70%', lineHeight:'22px'}}>
+        <h2 style={{wordWrap:'break-word'}}>Name:</h2>
+        <p>Type:
+          <br/>
+          Quantity:
+          <br/>
+          Giá:
+          <br/>
+          Phí xử lý:
+          <br/>
+          Phí phụ thu:
+          <br/>
+          <span style={{ fontWeight: 'bold' }}>Tổng cộng:</span>
+          </p>
       </div>
     </div>
   )
 }
+
