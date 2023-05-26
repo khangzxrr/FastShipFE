b4521938097d088c69dd4b1f8015f4fe13aabf0a
khangzxrr <khangzxrr@gmail.com>
2023-05-26 18:55:31 +0700

    add notifi utils

--- src/features/utils/Utils.js
+++ src/features/utils/Utils.js
@@ -3,6 +3,24 @@ export class Utils {
     static dateFormat = 'DD/MM/YYYY'
     static apiDateFormat = 'YYYY/MM/DD'
 
+    static showErrorNoti(messageApi, content){
+        messageApi.open({
+            type: 'error',
+            content
+        })
+    }
+
+    static showSuccessNoti(messageApi, content) {
+        messageApi.open({
+            type: 'success',
+            content
+        })
+    }
+
+    static showInfoNoti(messageApi, description) {
+        messageApi.info(description)
+    }
+
     static formatToVNDCurrency(currency){
 
         let vndFormat = Intl.NumberFormat('vi', {
