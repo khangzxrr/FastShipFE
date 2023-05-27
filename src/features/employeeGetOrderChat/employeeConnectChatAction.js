e9c08479db489a502df91048c23f96e369bd480d
khangzxrr <khangzxrr@gmail.com>
2023-05-28 03:47:13 +0700

    token get from getStates

--- src/features/employeeGetOrderChat/employeeConnectChatAction.js
+++ src/features/employeeGetOrderChat/employeeConnectChatAction.js
@@ -1,7 +1,14 @@
 import { HttpTransportType, HubConnectionBuilder } from '@microsoft/signalr';
 import { API_BASE_URL } from '../../features/axiosProfile';
+import { employeeGetOrderChatAction } from './employeeGetOrderChatAction';
 
-export const employeeConnectChatAction = (token) => {
+export const employeeConnectChatAction = (orderId) => async (dispatch, getState) => {
+
+    const states = getState()
+
+    const { token } = states.login
+
+
     const connectionBuilder = new HubConnectionBuilder()
         .withUrl(API_BASE_URL + "/chat", {
             skipNegotiation: false,
@@ -11,5 +18,19 @@ export const employeeConnectChatAction = (token) => {
         .withAutomaticReconnect()
         .build()
 
-    return connectionBuilder
+    await connectionBuilder.start()
+
+    console.log('connected ' + orderId)
+
+    await connectionBuilder.invoke('ConnectToChatRoom', {
+        orderId: parseInt(orderId)
+    })
+
+    connectionBuilder.on('boardcast', () => {
+        dispatch(employeeGetOrderChatAction(orderId))    
+    })
+
+    dispatch(employeeGetOrderChatAction(orderId))
+
+    return Promise.resolve(connectionBuilder)
 }
