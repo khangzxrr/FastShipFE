a278044c7d27d7d7a568a5856fd991369028d5e3
khangzxrr <khangzxrr@gmail.com>
2023-05-28 03:46:08 +0700

    add api data

--- src/component/request-employee/EmployeeRequestInfo.js
+++ src/component/request-employee/EmployeeRequestInfo.js
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Descriptions } from 'antd'
-export default function EmployeeRequestInfo() {
+export default function EmployeeRequestInfo(props) {
   return (
     <Descriptions>
-    <Descriptions.Item label="Customer Name" span={3}>Zhou Maomao</Descriptions.Item>
-    <Descriptions.Item label="DateTime Request" span={3}>14/12</Descriptions.Item>
-    <Descriptions.Item label="Phone Number" span={3}>0123</Descriptions.Item>
+    <Descriptions.Item label="Customer Name" span={3}>{props.order.customerName}</Descriptions.Item>
+    <Descriptions.Item label="DateTime Request" span={3}>{props.order.orderDate}</Descriptions.Item>
+    <Descriptions.Item label="Phone Number" span={3}>{props.order.contactPhoneNumber}</Descriptions.Item>
+    <Descriptions.Item label="customerDescription" span={3}>{props.order.customerDescription}</Descriptions.Item>
   </Descriptions>
   )
 }
