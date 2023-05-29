4b711355e27b307f7926da027cbbd42d2f0626d2
khangzxrr <khangzxrr@gmail.com>
2023-05-29 14:11:08 +0700

    add info fastbuy

--- src/component/fastbuy/FastBuyList.js
+++ src/component/fastbuy/FastBuyList.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import { Card, List, Button } from 'antd'
+import { Card, List, Button, Image } from 'antd'
 export default function FastBuyList(props) {
   
+  function onPageChange(pageIndex) {
+    props.fetchResellProduct(pageIndex - 1)
+  }
+
   return (
     <>
       <List
+
+        pagination={{
+          defaultPageSize: 10,
+          onChange: onPageChange,
+          total: props.total
+        }}
+
         grid={{
           gutter: 16,
           column: 4,
@@ -16,6 +27,8 @@ export default function FastBuyList(props) {
             <Card title={item.title}>
               <p>Category {item.category}</p>
               <p>Name {item.name}</p>
+              <Image src={item.imageUrl} />
+              <p>Description {item.description}</p>
               <p>Price: {item.price}$</p>
               <Button type='primary' style={{color:'black', width:'100%'}}>Buy</Button>
             </Card>
