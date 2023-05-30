04f0191a107939a8a9f08aac67cb3e8046c48e1b
khangzxrr <khangzxrr@gmail.com>
2023-05-31 00:47:55 +0700

    create payment remove token

--- src/features/createPayment/createPaymentAction.js
+++ src/features/createPayment/createPaymentAction.js
@@ -2,20 +2,10 @@ import axiosProfile from "../axiosProfile"
 
 export const createPaymentAction = (OrderId, callbackUrl) => async (dispatch, getState) => {
 
-    const states = getState()
-
-    const { token } = states.login
-
-    console.log(token)
-
     const response = await axiosProfile.get('/orders/pay', {
         params: {
             OrderId,
             Hostname: callbackUrl
-        },
-        headers: {
-            "Content-type": "application/json",
-            "Authorization": `Bearer ${token}`
         }
     })
 
