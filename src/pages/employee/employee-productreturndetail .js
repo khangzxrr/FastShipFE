c17bc5e29d41dad685712d8cadcbb3caf489a4c7
khangzxrr <khangzxrr@gmail.com>
2023-06-01 04:32:16 +0700

    add product issue api

--- src/pages/employee/employee-productreturndetail .js
+++ src/pages/employee/employee-productreturndetail .js
@@ -1,18 +1,23 @@
+import { useSelector } from "react-redux"
 import InforReturnDetail from "../../component/orders-employee/InforReturnDetail"
 import ProductReturnDetail from "../../component/orders-employee/ProductReturnDetail"
 import UpdateStatusReturn from "../../component/orders-employee/UpdateStatusReturn"
 const EmployeeProductReturnDetail = () => {
+
+    const { productIssue } = useSelector(state => state.employeeUpdateProductIssue)
+
+    console.log(productIssue)
     return (
         <div className="container" style={{ display: 'flex', marginTop: '20px', marginBottom: '20px' }}>
             <div style={{ width: '60%', marginRight: '2%', padding: '0px 20px 30px 20px' }}>
                 <h2>CHI TIẾT ĐƠN HÀNG</h2>
-                <ProductReturnDetail />
+                <ProductReturnDetail product={productIssue.productRecord}/>
             </div>
             <div style={{ width: '38%', padding: '0px 10px 10px 10px' }}>
                 <h2>THÔNG TIN ĐƠN HÀNG</h2>
-                <InforReturnDetail />
-                <h2>CẬP NHẬT TRẠNG THÁI ĐƠN HÀNG</h2>
-                <UpdateStatusReturn />
+                <InforReturnDetail productIssue={productIssue}/>
+                <h2>CẬP NHẬT TRẠNG THÁI YÊU CẦU</h2>
+                <UpdateStatusReturn productIssue={productIssue}/>
             </div>
         </div>
     )
