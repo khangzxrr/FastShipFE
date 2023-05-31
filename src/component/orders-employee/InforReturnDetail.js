a2dd1b9c46d68d5d19e62395836310f39f283d81
khangzxrr <khangzxrr@gmail.com>
2023-06-01 04:29:42 +0700

    add api user info

--- src/component/orders-employee/InforReturnDetail.js
+++ src/component/orders-employee/InforReturnDetail.js
@@ -1,11 +1,13 @@
 import React from 'react'
 import { Descriptions } from 'antd'
-export default function InforReturnDetail() {
+export default function InforReturnDetail(props) {
   return (
     <Descriptions>
-    <Descriptions.Item label="Tên khách hàng" span={3}></Descriptions.Item>
-    <Descriptions.Item label="SĐT" span={3}></Descriptions.Item>
-    <Descriptions.Item label="Dịch vụ" span={3}></Descriptions.Item>
+    <Descriptions.Item label="Tên khách hàng" span={3}>{props.productIssue.customerFullname}</Descriptions.Item>
+    <Descriptions.Item label="SĐT" span={3}>{props.productIssue.customerPhonenumber}</Descriptions.Item>
+    <Descriptions.Item label="Email" span={3}>{props.productIssue.customerEmail}</Descriptions.Item>
+    <Descriptions.Item label="Lí do đổi trả" span={3}>{props.productIssue.returnReason}</Descriptions.Item>
+    <Descriptions.Item label="Series" span={3}>{props.productIssue.series}</Descriptions.Item>
   </Descriptions>
   )
 }
