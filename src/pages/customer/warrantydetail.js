46e2a1920bea51ee089f8f8811a59715324e7226
khangzxrr <khangzxrr@gmail.com>
2023-05-31 19:02:04 +0700

    display info producr return detail

--- src/pages/customer/warrantydetail.js
+++ src/pages/customer/warrantydetail.js
@@ -1,31 +1,59 @@
+import { useNavigate, useSearchParams } from "react-router-dom"
 import ChatWarranty from "../../component/warranty/ChatWarranty"
 import ProductWarrantyDetail from "../../component/warranty/ProductWarrantyDetail"
 import StepWarranty from "../../component/warranty/StepWarranty"
 import { Descriptions } from "antd"
+import { useEffect, useState } from "react"
+import { useDispatch } from "react-redux"
+import { getProductReturnByIdAction } from "../../features/getProductReturnById/getProductReturnByIdAction"
+import StepReturn from "../../component/warranty/StepReturn"
 const WarrantyDetail = () => {
+
+    const [search, setSearch] = useSearchParams()
+
+    const [productReturn, setProductReturn] = useState({
+        productRecord: {}
+    })
+
+    const navigate = useNavigate()
+    const dispatch = useDispatch()
+
+    useEffect(() => {
+        const id = search.get('id')
+
+        if (id === null) {
+            navigate('/product-return-list')
+        }
+
+        dispatch(getProductReturnByIdAction(id))
+            .then(response => {
+                console.log(response)
+                setProductReturn(response.productReturnRecord)
+
+                console.log(productReturn.productRecord)
+            })
+    }, [])
+
     return (
         <>
             <div className='container' style={{ marginTop: '50px' }}>
-                <StepWarranty />
+                {
+                    productReturn.isWarranty ? (<StepWarranty status={productReturn.statusCode} />) : (<StepReturn status={productReturn.statusCode} />)
+                }
+                
             </div>
             <div className="container" style={{ display: 'flex', marginTop: '20px', marginBottom: '20px' }}>
                 <div style={{ width: '60%', marginRight: '2%', padding: '0px 20px 30px 20px' }}>
-                    <h2>CHI TIẾT ĐƠN HÀNG</h2>
-                    <ProductWarrantyDetail />
+                    <h2>CHI TIẾT ĐƠN HÀNG ĐANG ĐƯỢC BẢO HÀNH / ĐỔI TRẢ</h2>
+                    <ProductWarrantyDetail product={productReturn.productRecord}/>
                 </div>
                 <div style={{ width: '38%', padding: '0px 10px 10px 10px' }}>
                     <h2>THÔNG TIN KHÁCH HÀNG</h2>
                     <Descriptions>
-                        <Descriptions.Item label="Tên KH" span={3}></Descriptions.Item>
-                        <Descriptions.Item label="SĐT" span={3}></Descriptions.Item>
-                    </Descriptions>
-                    <h2>THÔNG TIN ĐƠN HÀNG</h2>
-                    <Descriptions>
-                        <Descriptions.Item span={3} label="Ngày tạo đơn"></Descriptions.Item>
-                        <Descriptions.Item span={3} label="Dịch vụ"></Descriptions.Item>
-                        <Descriptions.Item span={3} label="Địa chỉ"></Descriptions.Item>
-                        <Descriptions.Item span={3} label="SĐT"></Descriptions.Item>
-                        <Descriptions.Item span={3} label="Thời gian vận chuyển"></Descriptions.Item>
+                        <Descriptions.Item label="Status" span={3}>{productReturn.status}</Descriptions.Item>
+                        <Descriptions.Item label="returnDate" span={3}>{productReturn.returnDate}</Descriptions.Item>
+                        <Descriptions.Item label="returnReason" span={3}>{productReturn.returnReason}</Descriptions.Item>
+                        <Descriptions.Item label="Thuộc loại" span={3}>{productReturn.isWarranty ? 'Bảo hành' : 'Đổi trả'}</Descriptions.Item>
                     </Descriptions>
                     <h2>NỘI DUNG TRAO ĐỔI</h2>
                     <ChatWarranty/>
