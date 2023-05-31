41cfcecc5627bf018eeffff9854deeccfca8a749
khangzxrr <khangzxrr@gmail.com>
2023-06-01 04:30:51 +0700

    add product issue info component

---
+++ src/component/warranty/RequestProductIssueInfo.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+export default function RequestProductIssueInfo(props) {
+    return (
+        <div className='requestproducts' style={{ display: 'flex', padding: '10px', width: '100%' }}>
+            <img
+                src={props.product.imageUrl}
+                style={{
+                    height: '150px',
+                    width: '40%',
+                    marginTop: '20px'
+                    , boxShadow: 'rgba(0, 0, 0, 0.15) 1.95px 1.95px 2.6px'
+                }}
+            />
+            <div style={{ padding: '0px 10px', width: '60%' }}>
+                <div>
+                    <h2>{props.product.name}</h2>
+                    <a href={props.product.url}>Link Product</a><br />
+                    <p>Số lượng đã mua: {props.quantity}</p>
+                    <p>Bảo hành: {props.product.warrantable ? 'Có' : 'Không'}</p>
+                    {props.product.warrantable &&
+                        (<span>Mô tả bảo hành: {props.product.warrantableDescription}</span>)}
+                    <p>Đổi trả: {props.product.returnable ? 'Có' : 'Không'}</p>
+                    {props.product.returnable &&
+                        (<p>Mô tả đổi trả: Chấp nhận {props.product.returnDuration} ngày đổi trả</p>)}
+                </div>
+            </div>
+        </div>
+    )
+}
