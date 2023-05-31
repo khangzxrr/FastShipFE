a9d21279ebc5b1fb083f5cb7c0bf2d7c650ef560
khangzxrr <khangzxrr@gmail.com>
2023-06-01 04:30:26 +0700

    add api handle product issue status and finish

--- src/component/orders-employee/UpdateStatusReturn.js
+++ src/component/orders-employee/UpdateStatusReturn.js
@@ -1,20 +1,48 @@
 import React from 'react'
-import { Descriptions } from 'antd'
-export default function UpdateStatusReturn() {
+import { Button, Descriptions, Popconfirm, Select } from 'antd'
+import { useDispatch } from 'react-redux'
+import { employeeUpdateProductIssueStatusAction } from '../../features/employeeUpdateProductIssue/employeeUpdateProductIssueStatusAction'
+import { employeeUpdateProductIssueFinishStatusAction } from '../../features/employeeUpdateProductIssue/employeeUpdateProductIssueFinishStatusAction'
+export default function UpdateStatusReturn(props) {
+
+  const dispatch = useDispatch()
+
+  function onStatusChange(newStatus) {
+    dispatch(employeeUpdateProductIssueStatusAction(props.productIssue.id, newStatus))
+  }
+
+  function handleRefundAndClose() {
+    dispatch(employeeUpdateProductIssueFinishStatusAction(props.productIssue.id))
+  }
+
   return (
-    <Descriptions>
+    <div>
+<Descriptions>
                 <Descriptions.Item label="Status" span={3} >
-                    <select style={{ border: '1px solid grey', borderRadius: '10px', padding: '5px' }} >
-                        <option value={"noPayYet"}> Chưa thanh toán </option>
-                        <option value={"waitingToOrderFromSeller"}> Đang đợi order hàng</option>
-                        <option value={"orderingFromSeller"}> Đang mua hàng từ người bán</option>
-                        <option value={"deliverFromUsToVN"}> Đang giao từ US về VN</option>
-                        <option value={"inVNwarehouse"}> Đang ở kho VN</option>
-                        <option value={"deliverToCustomer"}> Đang giao đến khách hàng</option>
-                        <option value={"finished"}> Đã hoàn thành</option>
-                        <option value={"reselling"}> Bán lại đơn</option>
-                    </select>
+                    <Select onChange={onStatusChange} disabled={props.productIssue.finishStatus === 'processed'} value={props.productIssue.status} style={{ width: 250, border: '1px solid grey', borderRadius: '10px', padding: '5px' }} 
+                      options={[
+                        { value: 'request', label: 'Đang yêu cầu' },
+                        { value: 'acceptReturnEmployeeFault', label: 'Đã chấp nhận, lỗi do nhân viên' },
+                        { value: 'acceptReturnCustomerFault', label: 'Đã chấp nhận, lỗi do người dùng' },
+                        { value: 'acceptReturnSellerFault', label: 'Đã chấp nhận, lỗi do bên bán' },
+                      ]}
+                    />
                 </Descriptions.Item>
             </Descriptions>
+
+            <Popconfirm    
+                        disabled={props.productIssue.finishStatus === 'processed'}
+                        title="Xác nhận đóng"
+                        description="Bạn có chắc chắn muốn hoàn tiền và đóng yêu cầu này?"
+                        onConfirm={() => handleRefundAndClose()}
+                    >
+            <Button disabled={props.productIssue.finishStatus === 'processed'} type="primary" style={{  color: 'black', fontWeight: 600, margin: '5px 0px' }}>
+              {props.productIssue.finishStatus === 'processed' ? 'Đã hoàn tiền và đóng yêu cầu thành công' : 'Hoàn tiền cho người dùng và đóng yêu cầu'}
+            </Button>
+            </Popconfirm>
+    </div>
+    
+
+    
   )
 }
