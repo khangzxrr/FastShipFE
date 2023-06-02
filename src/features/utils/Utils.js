b415e8c0da4532d9105c36bd3ecb5de2ae44727a
khangzxrr <khangzxrr@gmail.com>
2023-06-02 15:04:30 +0700

    add translate issue status

--- src/features/utils/Utils.js
+++ src/features/utils/Utils.js
@@ -3,6 +3,26 @@ export class Utils {
     static dateFormat = 'DD/MM/YYYY'
     static apiDateFormat = 'YYYY/MM/DD'
 
+    static translateProductIssueState(state) {
+        const translate = {
+            'request': 'Đang yêu cầu',
+            'acceptEmployeeFault': 'chấp nhận, lỗi do nhân viên',
+            'acceptCustomerFault': 'chấp nhận, lỗi do khách',
+            'acceptSellerFault': 'chấp nhận, lỗi do người bán',
+            'exchangeforNew': 'Đổi hàng mới',
+            'sentToSeller': 'Đã gửi hàng đến người bán',
+            'successExchangeReturnToVN': 'Đổi mới thành công',
+            'failedExchangeSellerRejectReturnToVN': 'Đổi mới thất bại',
+            'refund': 'Hoàn tiền',
+            'shippingToCustomer': 'ship đến khách',
+            'shipperReceived': 'shipper đã nhận đơn',
+            'customerReceived': 'Người dùng đã nhận',
+            'finish': 'Hoàn thành'
+        }
+
+        return translate[state]
+    }
+
     static showErrorNoti(messageApi, content){
         messageApi.open({
             type: 'error',
