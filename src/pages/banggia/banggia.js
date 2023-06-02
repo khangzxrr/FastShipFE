60fef32a1c7ac1ebcf42a5483b618aa7635e8a7e
kduy1412 <kduy1412@gmail.com>
2023-06-02 22:59:11 +0700

    edit icon fe

--- src/pages/banggia/banggia.js
+++ src/pages/banggia/banggia.js
@@ -2,7 +2,8 @@ import { Table } from "antd"
 const BangGia = () => {
 
     return (
-        <div className="">
+        <div className="container">
+            <h2>Bảng giá cước dịch vụ</h2>
             <Table/>
         </div>
     )
