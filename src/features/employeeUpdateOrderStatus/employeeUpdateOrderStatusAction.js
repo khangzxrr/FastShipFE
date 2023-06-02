9f9a1c695139354863cc627cff7d37a0720442ea
khangzxrr <khangzxrr@gmail.com>
2023-06-03 05:35:00 +0700

    deny description

--- src/features/employeeUpdateOrderStatus/employeeUpdateOrderStatusAction.js
+++ src/features/employeeUpdateOrderStatus/employeeUpdateOrderStatusAction.js
@@ -2,11 +2,12 @@ import axiosProfile from "../axiosProfile"
 import { employeeGetOrderByIdAction } from "../employeeGetOrderById/employeeGetOrderByIdAction"
 
 
-export const employeeUpdateOrderStatusAction = (orderId, orderStatus) => async dispatch => {
+export const employeeUpdateOrderStatusAction = (orderId, orderStatus, orderDescription = null) => async dispatch => {
     
     const response = await axiosProfile.post('/employee/order/updateStatus', {
         orderId,
-        orderStatus
+        orderStatus,
+        orderDescription
     })
     
     dispatch(employeeGetOrderByIdAction(orderId))
