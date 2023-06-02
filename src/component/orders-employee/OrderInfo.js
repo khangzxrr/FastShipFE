bbe4787d336f233a7ffa34f3f7a254a825fb0f0a
khangzxrr <khangzxrr@gmail.com>
2023-06-03 05:33:48 +0700

    add loading, state constant

--- src/component/orders-employee/OrderInfo.js
+++ src/component/orders-employee/OrderInfo.js
@@ -1,15 +1,17 @@
 import React, { useEffect, useState } from 'react'
 import {
     Button,
-    Descriptions, Popconfirm
+    Descriptions, Popconfirm, Select, message
 } from 'antd'
 import { useDispatch } from 'react-redux'
 import { employeeUpdateOrderStatusAction } from '../../features/employeeUpdateOrderStatus/employeeUpdateOrderStatusAction'
 import { employeeMarkResellOrderAction } from '../../features/employeeMarkResellOrder/employeeMarkResellOrderAction'
+import { Utils } from '../../features/utils/Utils'
 
 export default function OrderInfo(props) {
 
-
+    const [loading, setLoading] = useState(false)
+    const [messageApi, messageContextHolder] = message.useMessage()
     const [currentStatus, setCurrentStatus] = useState("noPayYet")
 
 
@@ -19,16 +21,20 @@ export default function OrderInfo(props) {
         setCurrentStatus(props.order.status)
     }, [props.order])
 
-    function onStatusChange(e) {
+    function onStatusChange(value) {
 
-        setCurrentStatus(e.target.value)
+        setLoading(true)
+        setCurrentStatus(value)
 
-        dispatch(employeeUpdateOrderStatusAction(props.order.orderId, e.target.value))
+        dispatch(employeeUpdateOrderStatusAction(props.order.orderId, value))
             .then(() => {
-                
+                Utils.showSuccessNoti(messageApi, 'Cập nhật trạng thái thành công')
             })
             .catch(err => {
-                alert(err)
+                Utils.showErrorNoti(messageApi, 'Cập nhật trạng thái thất bại')
+            })
+            .finally(() => {
+                setLoading(false)
             })
 
     }
@@ -39,31 +45,40 @@ export default function OrderInfo(props) {
 
     return (
         <>
+        {messageContextHolder}
             <Descriptions>
                 <Descriptions.Item label="Trạng thái" span={3} >
-                    <select style={{ border: '1px solid grey', borderRadius: '10px', padding: '5px' }} onChange={onStatusChange} value={currentStatus}>
-                        <option value={"noPayYet"}> Chưa thanh toán </option>
-                        <option value={"waitingToOrderFromSeller"}> Đang đợi order hàng</option>
-                        <option value={"orderingFromSeller"}> Đang mua hàng từ người bán</option>
-                        <option value={"deliverFromUsToVN"}> Đang giao từ US về VN</option>
-                        <option value={"inVNwarehouse"}> Đang ở kho VN</option>
-                        <option value={"deliverToCustomer"}> Đang giao đến khách hàng</option>
-                        <option value={"finished"}> Đã hoàn thành</option>
-                        <option value={"reselling"}> Bán lại đơn</option>
-                    </select>
+                    <Select 
+                        style={{width: 300}}
+                        value={currentStatus}
+                        disabled={loading}
+                        loading={loading}
+                        onChange={onStatusChange}
+                        options={[
+                            { value: 'noPayYet', label: 'Chưa thanh toán', disabled: true},
+                            { value: 'waitingToOrderFromSeller', label: 'Đang đợi order hàng', disabled: true },
+                            { value: 'orderingFromSeller', label: 'Đang mua hàng từ người bán', disabled: currentStatus !== 'waitingToOrderFromSeller'},
+                            { value: 'deliverFromUsToVN', label: 'Đang giao từ US về VN', disabled: currentStatus !== 'orderingFromSeller'},
+                            { value: 'inVNwarehouse', label: 'Đang ở kho VN', disabled: currentStatus !== 'deliverFromUsToVN'},
+                            { value: 'deliverToCustomer', label: 'Đang giao đến khách hàng', disabled: currentStatus !== 'inVNwarehouse' },
+                            { value: 'finished', label: 'Đã hoàn thành', disabled: currentStatus !== 'inVNwarehouse'},
+                            { value: 'reselling', label: 'Bán lại đơn', disabled: currentStatus !== 'finished' }
+                        ]}
+                    
+                    />
                 </Descriptions.Item>
             </Descriptions>
 
             {
-                props.order.status !== 'reselling' &&
                 (
-                    <Popconfirm    
+                    <Popconfirm   
+                        disabled={currentStatus !== 'finished'} 
                         title="Xác nhận bán lại"
                         description="Bạn có chắc chắn muốn bán lại tất cả sản phẩm của đơn này?"
                         onConfirm={() => handleResell()}
                     >
 
-                        <Button type="primary" style={{color:'black'}}>Bán lại đơn hàng này</Button>
+                        <Button disabled={currentStatus !== 'finished'}  type="primary" style={{color:'black'}}>Bán lại đơn hàng này</Button>
                     </Popconfirm>
                 )
             }
