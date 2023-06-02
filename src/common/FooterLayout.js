e22724d9a5e43176c22576d4b5df70cc05dbc4ea
kduy1412 <kduy1412@gmail.com>
2023-06-02 16:00:35 +0700

    fe

--- src/common/FooterLayout.js
+++ src/common/FooterLayout.js
@@ -10,17 +10,18 @@ export const FooterLayout = () => {
     return (
         <Footer className="footer" style={{ padding: "0px" }}>
             <div style={{ backgroundColor: "white", padding: "20px 0", boxShadow: 'rgba(0, 0, 0, 0.16) 0px 1px 4px' }}>
-                <div style={{ color: "black", display: "flex", padding:'0% 5%' }}>
+                <div style={{ color: "black", display: "flex", padding:'0% 10%' }}>
                     <Row gutter={[24, 8]}>
                         <Col style={{ fontSize: 24, fontWeight: 600 }} span={8}>
                             Tài khoản
                         </Col>
                         <Col style={{ fontSize: 24, fontWeight: 600 }} span={8}>
-                            Thông tin liên hệ
-                        </Col>
-                        <Col style={{ fontSize: 24, fontWeight: 600 }} span={8}>
                             Chính sách
                         </Col>
+                        <Col style={{ fontSize: 24, fontWeight: 600 }} span={8}>
+                            Thông tin liên hệ
+                        </Col>
+                       
                         <Col span={8}>
                             <div className="links">
                                 <div>
@@ -43,22 +44,6 @@ export const FooterLayout = () => {
                         <Col span={8}>
                             <div className="links">
                                 <div>
-                                   <ImLocation/> <a>Lô E2a-7, Đường D1 Khu Công nghệ cao, P. Long Thạnh Mỹ, TP. Thủ Đức, TP. Hồ Chí Minh</a>
-                                </div>
-                                <div>
-                                   <AiFillPhone/> <a>028 7300 1866</a>
-                                </div>
-                                <div>
-                                    <AiOutlineMail/> <a>hotro@fastship.com</a>
-                                </div>
-                                <div>
-                                   <AiFillFacebook/> <a>FastShip.com Facebook</a>
-                                </div>
-                            </div>
-                        </Col>
-                        <Col span={8}>
-                            <div className="links">
-                                <div>
                                  <AiOutlineCheck/> <a>Điều khoản dịch vụ</a>
                                 </div>
                                 <div>
@@ -69,6 +54,22 @@ export const FooterLayout = () => {
                                 </div>
                                 <div>
                                 <AiOutlineCheck/> <a>Chính sách riêng</a>
+                                </div>
+                            </div>
+                        </Col>
+                        <Col span={8}>
+                            <div className="links">
+                                <div>
+                                   <ImLocation/> <a>Lô E2a-7, Đường D1 Khu Công nghệ cao, P. Long Thạnh Mỹ, TP. Thủ Đức, TP. Hồ Chí Minh</a>
+                                </div>
+                                <div>
+                                   <AiFillPhone/> <a>028 7300 1866</a>
+                                </div>
+                                <div>
+                                    <AiOutlineMail/> <a>hotro@fastship.com</a>
+                                </div>
+                                <div>
+                                   <AiFillFacebook/> <a>FastShip.com Facebook</a>
                                 </div>
                             </div>
                         </Col>
