f0e31412f757ad114d14369c13176c57a0881c9b
khangzxrr <khangzxrr@gmail.com>
2023-06-02 15:03:44 +0700

    UpdateStatusReturn fetch nextstages and update sta

--- src/component/orders-employee/UpdateStatusReturn.js
+++ src/component/orders-employee/UpdateStatusReturn.js
@@ -1,48 +1,98 @@
-import React from 'react'
-import { Button, Descriptions, Popconfirm, Select } from 'antd'
+import React, { useEffect, useState } from 'react'
+import { Button, Descriptions, Popconfirm, Select, message } from 'antd'
 import { useDispatch } from 'react-redux'
 import { employeeUpdateProductIssueStatusAction } from '../../features/employeeUpdateProductIssue/employeeUpdateProductIssueStatusAction'
-import { employeeUpdateProductIssueFinishStatusAction } from '../../features/employeeUpdateProductIssue/employeeUpdateProductIssueFinishStatusAction'
+import { employeeGetNextProductIssueStateAction } from '../../features/employeeGetNextProductIssueState/employeeGetNextProductIssueStateAction'
+import { Utils } from '../../features/utils/Utils'
 export default function UpdateStatusReturn(props) {
+
+  const [messageApi, messaegContextHolder] = message.useMessage()
+
+  
+
+  const [selectOptions, setSelectOptions] = useState([])
+  const [newStatus, setNewStatus] = useState(props.productIssue.status)
 
   const dispatch = useDispatch()
 
-  function onStatusChange(newStatus) {
-    dispatch(employeeUpdateProductIssueStatusAction(props.productIssue.id, newStatus))
+  function onStatusChange(selectedStatus) {
+    setNewStatus(selectedStatus)
   }
 
-  function handleRefundAndClose() {
-    dispatch(employeeUpdateProductIssueFinishStatusAction(props.productIssue.id))
+  function fetchNextStates() {
+    dispatch(employeeGetNextProductIssueStateAction(props.productIssue.id))
+      .then(response => {
+
+        let mapToSelect = []
+
+        for (let i in response.nextState) {
+          mapToSelect.push({
+            value: response.nextState[i],
+            label: Utils.translateProductIssueState(response.nextState[i])
+          })
+
+        }
+
+
+
+        console.log(mapToSelect)
+        setSelectOptions(mapToSelect)
+
+        if (props.productIssue.status !== 'finish'){
+          setNewStatus(mapToSelect[0].value)
+        } 
+        
+      })
   }
+
+  function handleSaveChanges() {
+    dispatch(employeeUpdateProductIssueStatusAction(props.productIssue.id, newStatus))
+      .then(resposne => {
+        Utils.showSuccessNoti(messageApi, 'Cập nhật trạng thái thành công')
+      })
+      .catch(err => {
+        Utils.showErrorNoti(messageApi, 'Lỗi khi cập nhật trạng thái')
+      })
+      .finally(() => {
+        fetchNextStates()
+      })
+  }
+
+  useEffect(() => {
+    fetchNextStates()
+  }, [])
 
   return (
     <div>
-<Descriptions>
-                <Descriptions.Item label="Status" span={3} >
-                    <Select onChange={onStatusChange} disabled={props.productIssue.finishStatus === 'processed'} value={props.productIssue.status} style={{ width: 250, border: '1px solid grey', borderRadius: '10px', padding: '5px' }} 
-                      options={[
-                        { value: 'request', label: 'Đang yêu cầu' },
-                        { value: 'acceptReturnEmployeeFault', label: 'Đã chấp nhận, lỗi do nhân viên' },
-                        { value: 'acceptReturnCustomerFault', label: 'Đã chấp nhận, lỗi do người dùng' },
-                        { value: 'acceptReturnSellerFault', label: 'Đã chấp nhận, lỗi do bên bán' },
-                      ]}
-                    />
-                </Descriptions.Item>
-            </Descriptions>
+      {messaegContextHolder}
+      
+      Trạng thái đã cập nhật trước đó
+      {
+        props.productIssue.stateTracking.map(state => (<p>{Utils.translateProductIssueState(state.status)}  -  {state.changeDate}</p>))
+      }
 
-            <Popconfirm    
-                        disabled={props.productIssue.finishStatus === 'processed'}
-                        title="Xác nhận đóng"
-                        description="Bạn có chắc chắn muốn hoàn tiền và đóng yêu cầu này?"
-                        onConfirm={() => handleRefundAndClose()}
-                    >
-            <Button disabled={props.productIssue.finishStatus === 'processed'} type="primary" style={{  color: 'black', fontWeight: 600, margin: '5px 0px' }}>
-              {props.productIssue.finishStatus === 'processed' ? 'Đã hoàn tiền và đóng yêu cầu thành công' : 'Hoàn tiền cho người dùng và đóng yêu cầu'}
-            </Button>
-            </Popconfirm>
+
+      <Descriptions>
+        <Descriptions.Item label="Status" span={3} >
+          <Select onChange={onStatusChange} disabled={props.productIssue.status === 'finish' || props.productIssue.status === 'shippingToCustomer'} value={newStatus} style={{ width: 250, border: '1px solid grey', borderRadius: '10px', padding: '5px' }}
+            options={selectOptions}
+          />
+        </Descriptions.Item>
+      </Descriptions>
+
+      <Popconfirm
+        disabled={props.productIssue.status === 'finish' || props.productIssue.status === newStatus || props.productIssue.status === 'shippingToCustomer'}
+        title="Xác nhận"
+        description="Bạn có chắc chắn muốn lưu và thực hiện tác vụ này?"
+        onConfirm={() => handleSaveChanges()}
+      >
+        <Button disabled={props.productIssue.status === 'finish' || props.productIssue.status === newStatus || props.productIssue.status === 'shippingToCustomer'} type="primary" style={{ color: 'black', fontWeight: 600, margin: '5px 0px' }}>
+          {props.productIssue.status === 'finish' ? 'Đã xử lí thành công yêu cầu' : 'Lưu và thực hiện tác vụ'}
+        </Button>
+      </Popconfirm>
     </div>
-    
 
-    
+
+
   )
 }
