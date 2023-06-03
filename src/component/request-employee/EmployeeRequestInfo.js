7d3a4f47d17bfe4c579e845f7c16a44e149abf4f
khangzxrr <khangzxrr@gmail.com>
2023-06-03 16:27:56 +0700

    fix date

--- src/component/request-employee/EmployeeRequestInfo.js
+++ src/component/request-employee/EmployeeRequestInfo.js
@@ -2,6 +2,7 @@ import React from 'react'
 import { Descriptions, Button, Modal, Input } from 'antd'
 import { AiOutlineEdit } from 'react-icons/ai'
 import { useState } from 'react';
+import Moment from 'react-moment';
 export default function EmployeeRequestInfo(props) {
   const [isModalOpen, setIsModalOpen] = useState(false);
   const showModal = () => {
@@ -17,7 +18,7 @@ export default function EmployeeRequestInfo(props) {
     <>
       <Descriptions>
         <Descriptions.Item label="Tên khách hàng" span={3}>{props.order.customerName}</Descriptions.Item>
-        <Descriptions.Item label="Ngày yêu cầu" span={3}>{props.order.orderDate}</Descriptions.Item>
+        <Descriptions.Item label="Ngày yêu cầu" span={3}><Moment date={props.order.orderDate} format='DD/MM/YYYY HH:mm' /></Descriptions.Item>
         <Descriptions.Item label="SĐT" span={3}>{props.order.contactPhoneNumber}</Descriptions.Item>
         <Descriptions.Item label="Địa chỉ" span={3}>{props.order.deliveryAddress} <a  onClick={showModal}><AiOutlineEdit /></a></Descriptions.Item>
       </Descriptions>
