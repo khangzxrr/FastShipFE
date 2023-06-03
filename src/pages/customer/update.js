945a93395a42fa741bc3c50a70b43d2102dd622b
kduy1412 <kduy1412@gmail.com>
2023-06-03 16:37:08 +0700

    register fe

--- src/pages/customer/update.js
+++ src/pages/customer/update.js
@@ -1,45 +1,44 @@
 import UpdateProfile from "../../component/home/UpdateProfile"
-
 const Update = () => {
-    return (
-<div className="container">
-        <h1
-          style={{
-            fontSize: 20,
-            color: "black",
-            lineHeight: 1.35,
-            fontWeight: 400,
-          }}
-        >
-          Cập nhật tài khoản
-        </h1>
-        <div
-          style={{
-            display: "flex",
-            width: "100%",
-            padding: "20px 25px",
-            boxShadow: "0 2px 3px rgb(0 0 0 / 8%)",
-            borderRadius: "8px",
-            borderTop: "4px solid #FFE715",
-            marginBottom:"50px"
-          }}
-        >
-          <div style={{ position: "relative", width: "100%", padding: "0 15px" }}>
-            <h2
-              style={{
-                fontSize: 16,
-                color: "black",
-                lineHeight: 1.35,
-                fontWeight: 400,
-              }}
-            >
-              THÔNG TIN CÁ NHÂN
-            </h2>
-            <UpdateProfile/>
-          </div>
+  return (
+    <div className="container">
+      <h1
+        style={{
+          fontSize: 20,
+          color: "black",
+          lineHeight: 1.35,
+          fontWeight: 400,
+        }}
+      >
+        Cập nhật tài khoản
+      </h1>
+      <div
+        style={{
+          display: "flex",
+          width: "100%",
+          padding: "20px 25px",
+          boxShadow: "0 2px 3px rgb(0 0 0 / 8%)",
+          borderRadius: "8px",
+          borderTop: "4px solid #FFE715",
+          marginBottom: "50px"
+        }}
+      >
+        <div style={{ position: "relative", width: "100%", padding: "0 15px" }}>
+          <h2
+            style={{
+              fontSize: 16,
+              color: "black",
+              lineHeight: 1.35,
+              fontWeight: 400,
+            }}
+          >
+            THÔNG TIN CÁ NHÂN
+          </h2>
+          <UpdateProfile />
         </div>
       </div>
-        
-    )
+    </div>
+
+  )
 }
 export default Update
