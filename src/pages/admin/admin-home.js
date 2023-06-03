0973a386be092178f4a660614a6d1da06a540b0e
khangzxrr <khangzxrr@gmail.com>
2023-06-03 14:05:29 +0700

    redirect on wrong role

--- src/pages/admin/admin-home.js
+++ src/pages/admin/admin-home.js
@@ -1,8 +1,25 @@
+import { useDispatch, useSelector } from "react-redux"
 import AccountManagement from "../../component/admin/AccountManagement"
 import AdminMenu from "../../component/admin/AdminMenu"
 import ServerReport from "../../component/admin/ServiceReport"
 import "../admin/admin.css"
+import { useEffect } from "react"
+import { logout } from "../../features/login/loginSlice"
+import { useNavigate } from "react-router-dom"
 const AdminHome = () => {
+
+    const { roleName } = useSelector(state => state.login)
+
+    const dispatch = useDispatch()
+    const navigate = useNavigate()
+
+    useEffect(() => {
+        if (roleName !== 'ADMIN') {
+            dispatch(logout())
+            navigate('/login')
+            return
+        }
+    }, [roleName])
     return (
         <div className='container'>
             <AdminMenu/>
