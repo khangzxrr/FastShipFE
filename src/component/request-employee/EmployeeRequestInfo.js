e966117062b67c502b4172c7550576891855e0e8
khangzxrr <khangzxrr@gmail.com>
2023-06-03 18:55:23 +0700

    change address

--- src/component/request-employee/EmployeeRequestInfo.js
+++ src/component/request-employee/EmployeeRequestInfo.js
@@ -3,12 +3,33 @@ import { Descriptions, Button, Modal, Input } from 'antd'
 import { AiOutlineEdit } from 'react-icons/ai'
 import { useState } from 'react';
 import Moment from 'react-moment';
+import { useDispatch } from 'react-redux';
+import { employeeUpdateOrderAddressAction } from '../../features/employeeUpdateOrderAddress/employeeUpdateOrderAddressAction';
+import { Utils } from '../../features/utils/Utils';
 export default function EmployeeRequestInfo(props) {
   const [isModalOpen, setIsModalOpen] = useState(false);
+  const [address, setAddress] = useState(props.order.deliveryAddress)
+
+  const dispatch = useDispatch()
+
+  function onChangeAddress(event) {
+    setAddress(event.target.value)
+  }
+
   const showModal = () => {
+
     setIsModalOpen(true);
   };
   const handleOk = () => {
+
+    dispatch(employeeUpdateOrderAddressAction(props.order.orderId, address))
+    .then(res => {
+      Utils.showSuccessNoti(props.messageApi, 'Cập nhật thành công')
+    })
+    .catch(() => {
+      Utils.showErrorNoti(props.messageApi, 'Cập nhật thất bại')
+    })
+
     setIsModalOpen(false);
   };
   const handleCancel = () => {
@@ -23,7 +44,10 @@ export default function EmployeeRequestInfo(props) {
         <Descriptions.Item label="Địa chỉ" span={3}>{props.order.deliveryAddress} <a  onClick={showModal}><AiOutlineEdit /></a></Descriptions.Item>
       </Descriptions>
       <Modal title="Thay đổi địa chỉ" open={isModalOpen} onOk={handleOk} onCancel={handleCancel}>
-        <Input/>
+        <Input 
+          onChange={onChangeAddress}
+          value={address}
+        />
       </Modal>
     </>
   )
